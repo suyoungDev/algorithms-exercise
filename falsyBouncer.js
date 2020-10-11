1dc6dff5c6dd6a0d08a708f30733375209b8dc5b
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-10-11 12:13:55 +0900

    Create falsyBouncer.js
    if(arr[i]) // true일 때 반환하기
    arr.filter(Boolean) // Boolean도 내장함수: true일때 반환하기

---
+++ falsyBouncer.js
@@ -0,0 +1,22 @@
+// Remove all falsy values from an array.
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+// Hint: Try converting each value to a Boolean.
+
+function bouncer(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      // 이거 자체만으로도 falsy인지 truthy인지 알수있다!
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+bouncer([7, 'ate', '', false, 9]);
+
+// another answer
+
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
