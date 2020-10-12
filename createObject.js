85a1fe4652c043537ecdddc0ef4e89cfa324c94e
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-10-13 08:48:35 +0900

    learining obj

--- createObject.js
+++ createObject.js
@@ -26,3 +26,111 @@ for (let property in beagle) {
     prototypeProps.push(property);
   }
 }
+
+function joinBirdFraternity(candidate) {
+  if (candidate.constructor === Bird) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Bird.prototype.eat = function () {
+  console.log('nom nom nom');
+};
+
+Bird.prototype.describe = function () {
+  console.log('My name is ' + this.name);
+};
+
+Bird.prototype = {
+  numLegs: 2,
+  eat: function () {
+    console.log('nom nom nom');
+  },
+  describe: function () {
+    console.log('My name is ' + this.name);
+  },
+};
+
+Bird.prototype = {
+  constructor: Bird, // define the constructor property
+  numLegs: 2,
+  eat: function () {
+    console.log('nom nom nom');
+  },
+  describe: function () {
+    console.log('My name is ' + this.name);
+  },
+};
+
+function Bird(name) {
+  this.name = name;
+}
+
+let duck = new Bird('Donald');
+
+Bird.prototype.isPrototypeOf(duck);
+// returns true
+
+let animal = new Animal();
+
+let duck = Object.create(Animal.prototype); // Change this line
+
+Bird.prototype = Object.create(Animal.prototype);
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log('nom nom nom');
+  },
+};
+
+function Dog() {}
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
+beagle.eat();
+
+function Bird() {}
+Bird.prototype = Object.create(Animal.prototype);
+let duck = new Bird();
+duck.constructor; // function Animal(){...}
+
+Bird.prototype.constructor = Bird;
+duck.constructor; // function Bird(){...}
+
+function Animal() {}
+Animal.prototype.eat = function () {
+  console.log('nom nom nom');
+};
+function Bird() {}
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.constructor = Bird;
+
+Bird.prototype.fly = function () {
+  console.log("I'm flying!");
+};
+
+let duck = new Bird();
+duck.eat(); // prints "nom nom nom"
+duck.fly(); // prints "I'm flying!"
+
+function Animal() {}
+Animal.prototype.eat = function () {
+  return 'nom nom nom';
+};
+function Bird() {}
+
+// Inherit all methods from Animal
+Bird.prototype = Object.create(Animal.prototype);
+
+// Bird.eat() overrides Animal.eat()
+Bird.prototype.eat = function () {
+  return 'peck peck peck';
+};
