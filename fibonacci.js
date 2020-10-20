9dcf95882c36b7212b17739d2721fa0c10735b32
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-10-20 21:17:10 +0900

    fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -14,3 +14,39 @@ var fibonacci = function(result, len){
 
     return result;
 }
+
+
+// recursion
+
+function fib2(result, len){
+    if( result.length >= len ){
+        return result;
+    } 
+    result.push(result[result.length-2] + result[result.length - 1]);
+
+    return fib2(result, len);
+}
+
+
+// fibonacci odd number
+
+function sumFibs(num) {
+    let prev = 0;
+    let curr = 1;
+    let result = 0;
+  
+    while( curr <= num){
+      if( curr % 2 !== 0 ){
+        result += curr;
+      }
+  
+      curr = curr + prev;
+      
+      prev = curr - prev;
+  
+    }
+    return result;
+  }
+  
+  sumFibs(4);
+  
